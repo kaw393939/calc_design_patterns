a80c8204c7cfe8fdb72d8aa18cf3bc663f057a98
kaw393939 <kwilliam@njit.edu>
2024-02-19 13:18:02 -0500

    fixed test

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,5 +1,6 @@
 import pytest
 from app import App
+from app.commands.goodbye import GoodbyeCommand
 from app.commands.greet import GreetCommand
 
 def test_greet_command(capfd):
@@ -7,6 +8,12 @@ def test_greet_command(capfd):
     command.execute()
     out, err = capfd.readouterr()
     assert out == "Hello, World!\n", "The GreetCommand should print 'Hello, World!'"
+
+def test_goodbye_command(capfd):
+    command = GoodbyeCommand()
+    command.execute()
+    out, err = capfd.readouterr()
+    assert out == "Goodbye\n", "The GreetCommand should print 'Hello, World!'"
 
 
 def test_app_greet_command(capfd, monkeypatch):
