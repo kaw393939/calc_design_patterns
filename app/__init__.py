ab316e340aeabd577cb83d9495c193d592a9fbcb
richas-bsingh <72738297+richas0719@users.noreply.github.com>
2024-02-20 21:42:39 -0500

    Extend command fucntionality using kwargs for accepting user input

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,22 @@ class App:
         self.command_handler = CommandHandler()
         # Register commands here
 
-
     def start(self):
         self.command_handler.register_command("greet", GreetCommand())
         self.command_handler.register_command("goodbye", GoodbyeCommand())
         self.command_handler.register_command("exit", ExitCommand())
 
         print("Type 'exit' to exit.")
-        while True:  #REPL Read, Evaluate, Process, Loop
-            self.command_handler.execute_command(input(">>> ").strip())
+        while True: #REPL Read, Evaluate, Process, Loop
+            command_input = input(">>> ").strip()
+            command_parts = command_input.split()  # Split input into command and arguments
+            command_name = command_parts[0]
+            command_args = command_parts[1:]
 
+            # Convert command arguments to kwargs (assuming format: key=value)
+            kwargs = {}
+            for arg in command_args:
+                key, value = arg.split('=')
+                kwargs[key] = value
 
-
+            self.command_handler.execute_command(command_name, **kwargs)
