61420550910462566f9f92dae39ef1d7c4cb11f2
Vaidik Vyas <42435713+vaidikrvyas@users.noreply.github.com>
2024-02-22 11:08:56 -0500

    Update __init__.py

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class Command(ABC):
     @abstractmethod
-    def execute(self):
+    def execute(self, *args, **kwargs):
         pass
 
 class CommandHandler:
@@ -12,9 +12,26 @@ class CommandHandler:
     def register_command(self, command_name: str, command: Command):
         self.commands[command_name] = command
 
-    def execute_command(self, command_name: str):
+    def execute_command(self, command_name: str, *args, **kwargs):
         if command_name in self.commands:
-            self.commands[command_name].execute()
+
+            self.commands[command_name].execute(*args, **kwargs)
         else:
             print(f"No such command: {command_name}")
 
+class GreetCommand(Command):
+    def execute(self, *args, **kwargs):
+        name = kwargs.get('name', 'World')
+        print(f"Hello, {name}!")
+
+class AddCommand(Command):
+    def execute(self, *args, **kwargs):
+        total = sum(args)
+        print(f"The sum is: {total}")
+
+handler = CommandHandler()
+handler.register_command("greet", GreetCommand())
+handler.register_command("add", AddCommand())
+
+handler.execute_command("greet", name="Alice")
+handler.execute_command("add", 1, 2, 3)
