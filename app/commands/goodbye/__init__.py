28a44724eaa9a61ad0e4240e65a1cc630b3a4cdf
haren0612 <haren0612@gmail.com>
2024-02-22 01:01:58 -0500

    command implementation Args and Kwargs

--- app/commands/goodbye/__init__.py
+++ app/commands/goodbye/__init__.py
@@ -2,5 +2,6 @@ from app.commands import Command
 
 
 class GoodbyeCommand(Command):
-    def execute(self):
-        print("Goodbye")
+    def execute(self, *args, **kwargs):
+        name = kwargs.get('name')
+        print(f"Goodbye {name if name else 'friend'}")
