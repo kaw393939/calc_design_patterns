b1d4e265653d29d0d7984d4b945ebbeaaf14407c
k-0016 <kp866@njit.edu>
2024-02-22 02:45:22 -0500

    Add Changes

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class Command(ABC):
     @abstractmethod
-    def execute(self):
+    def execute(self, *args, **kwargs):
         pass
 
 class CommandHandler:
@@ -12,9 +12,8 @@ class CommandHandler:
     def register_command(self, command_name: str, command: Command):
         self.commands[command_name] = command
 
-    def execute_command(self, command_name: str):
+    def execute_command(self, command_name: str, *args, **kwargs):
         if command_name in self.commands:
-            self.commands[command_name].execute()
+            self.commands[command_name].execute(*args, **kwargs)
         else:
             print(f"No such command: {command_name}")
-
--- app/commands/goodbye/__init__.py
+++ app/commands/goodbye/__init__.py
@@ -1,6 +1,6 @@
 from app.commands import Command
 
-
 class GoodbyeCommand(Command):
-    def execute(self):
+    def execute(self, *args, **kwargs):
+        """Print a goodbye message."""
         print("Goodbye")
